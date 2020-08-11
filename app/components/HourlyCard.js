44f451c816a1fc05a33033765d28e67aafd5ff85
Manuel Meneses <manuelmenesesg@hotmail.com>
2020-08-10 23:21:39 -0400

    stylizing card

--- app/components/HourlyCard.js
+++ app/components/HourlyCard.js
@@ -1,12 +1,28 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default class HourlyCard extends Component {
-    render() {
-        return (
-            <View>
-                <Text> textInComponent </Text>
-            </View>
-        )
-    }
+  render() {
+    return (
+      <View style={styles.card}>
+        <Text style={[styles.cardText, { fontWeight: "bold" }]}> Now </Text>
+        <Text style={[styles.cardText, { fontWeight: "700" }]}> 32° </Text>
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  card: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#379392",
+    paddingVertical: 60,
+    paddingHorizontal: 45,
+    borderRadius: 20,
+  },
+  cardText: {
+    color: "#474787",
+    fontSize: 20,
+  },
+});
