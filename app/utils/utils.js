bfa51ff0d081fcd05dec372547f214f1659b77bd
Manuel Meneses <manuelmenesesg@hotmail.com>
2020-08-12 18:32:27 -0400

    Adding new helpers (whichBackground and filterHoursActualDay)

--- app/utils/utils.js
+++ app/utils/utils.js
@@ -1,7 +1,26 @@
-import { PureComponent } from "react";
+export const whichBackground = () => {
+  const date = new Date();
+  if (date.getHours() >= 4 && date.getHours() < 6) {
+    return require("../assets/img/dawn-bg.jpg");
+  }
+  if (date.getHours() >= 6 && date.getHours() < 3) {
+    return require("../assets/img/day-bg.jpg");
+  }
+  if (date.getHours() >= 2 && date.getHours() < 19) {
+    return require("../assets/img/noon-bg.jpg");
+  }
+  return require("../assets/img/night-bg.jpg");
+};
 
 export const kelvinToCelsius = (measure) => {
   return Number((measure - 273.15).toFixed(1));
+};
+
+export const filterHoursActualDay = (hourly) => {
+  const hours = hourly.filter((hour) => {
+    return new Date(hour.dt * 1000).getDay() === new Date().getDay();
+  });
+  return hours;
 };
 
 export const getHoursToday = (date) => {
@@ -13,35 +32,32 @@ export const getHoursToday = (date) => {
   return formatTime;
 };
 
-//Envia el main del objeto weather retorna un simbolo. 
+//Envia el main del objeto weather retorna un simbolo.
 export const getWeatherIcon = (weather) => {
   switch (weather) {
-      case 'Clear': 
-          return '☀';
-      case 'Clouds':
-          return '☁'
-      case 'Rain':
-          return '🌧';
-      case 'Thunderstorm':
-          return '⛈';
-      case 'Snow':
-          return '❄';
-      case 'Mist': 
-          return '🌫'
-      default: return '☁';
+    case "Clear":
+      return "☀";
+    case "Clouds":
+      return "☁";
+    case "Rain":
+      return "🌧";
+    case "Thunderstorm":
+      return "⛈";
+    case "Snow":
+      return "❄";
+    case "Mist":
+      return "🌫";
+    default:
+      return "☁";
   }
-}
+};
 
 export const getDayDateName = (date) => {
   return (
-      [
-          "Domingo",
-          "Lunes",
-          "Martes",
-          "Miercoles",
-          "Jueves",
-          "Viernes",
-          "Sábado",
-      ][date.getDay()] + ' ' + date.getDate()
+    ["Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sábado"][
+      date.getDay()
+    ] +
+    " " +
+    date.getDate()
   );
-}
+};
