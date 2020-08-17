5698c4fcf0001e58d8f9783bd57f8b3424b79d60
Manuel Meneses <manuelmenesesg@hotmail.com>
2020-08-17 15:57:59 -0400

    Adding set location feature

--- app/screen/SearchLocation.js
+++ app/screen/SearchLocation.js
@@ -1,81 +1,112 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, TextInput, FlatList } from 'react-native';
-import { AntDesign } from '@expo/vector-icons';
-import * as Location from "expo-location";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, TextInput, FlatList } from "react-native";
+import { StackActions } from '@react-navigation/native';
 
-import { searchLoaction } from '../api/index'
-import SearchRow from '../components/SearchRow';
-const Separator = () => <View style={styles.separator} />;
+// Expo imports
+import { AntDesign } from "@expo/vector-icons";
 
-export default function SearchLocation({ navigation, onLocationSelected }) {
+// API fetch
+import { searchLoaction } from "../api/index";
+import { weather } from "../api/index";
 
-    const [search, setSearch] = useState('');
-    const [places, setPlaces] = useState([])
-    
-    useEffect(() => {
-        const searchApi = async () => {
-            try {
-                const placesApi = await searchLoaction.getPlaces(search)
-                const filterPlaces = await placesApi.json()
-                setPlaces(filterPlaces.filter(pl => pl.address.state !== undefined))
-            } catch (error) {
-                console.log(error + ' error al tratar de buscar lugares')
-            }
-        }
-        searchApi()
-        return () => {
+// LocalStorgae
+import { localStorage } from "../storage/localStorage";
 
-        }
-    }, [search])
+// Components
+import ActivityIndicatorApp from '../components/ActivityIndicatorApp'
+import SearchRow from "../components/SearchRow";
 
-    const setLocation = (latitude, longitude) => {
-        onLocationSelected(latitude, longitude)
+export default function SearchLocation({ navigation }) {
+  const [search, setSearch] = useState("");
+  const [places, setPlaces] = useState([]);
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+    const searchApi = async () => {
+      try {
+        const placesApi = await searchLoaction.getPlaces(search);
+        const filterPlaces = await placesApi.json();
+        setPlaces(filterPlaces.filter((pl) => pl.address.state !== undefined));
+      } catch (error) {
+        console.log(error + " error al tratar de buscar lugares");
+      }
+    };
+    searchApi();
+  }, [search]);
+
+  const setLocation = async (latitude, longitude) => {
+    try {
+      setIsLoading(true)
+      const response = await weather.getWeather(latitude, longitude);
+      const { current, hourly, daily } = await response.json();
+      localStorage.setForecast({ current, hourly, daily });
+      setIsLoading(false)
+      navigation.dispatch(StackActions.replace("Home"));
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    const renderItem = ({ item }) => <SearchRow place={item.display_name} address={item.address} latitude={item.lat} longitude={item.lon} onPress={setLocation} />;
+  const renderItem = ({ item }) => (
+    <SearchRow
+      place={item.display_name}
+      address={item.address}
+      latitude={item.lat}
+      longitude={item.lon}
+      onPress={setLocation}
+    />
+  );
 
-    return (
-        <View style={styles.container}>
-            <View style={styles.input}>
-                <AntDesign name="search1" size={20} color="gray" />
-                <TextInput
-                    style={styles.textInputStyle}
-                    onChangeText={text => setSearch(text)}
-                    value={search}
-                    underlineColorAndroid="transparent"
-                    placeholder="Buscar (Ej. pais, estado, ciudad, calle, codigo postal)"
-                />
-            </View>
-            <FlatList
-                data={places}
-                renderItem={renderItem}
-                ItemSeparatorComponent={Separator}
-                keyExtractor={(item) => item.place_id + ''}
-            />
-        </View>
-    )
+  const Separator = () => <View style={styles.separator} />;
+
+  if(isLoading){
+    return <ActivityIndicatorApp />
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.input}>
+        <AntDesign name="search1" size={20} color="gray" />
+        <TextInput
+          style={styles.textInputStyle}
+          onChangeText={(text) => setSearch(text)}
+          value={search}
+          underlineColorAndroid="transparent"
+          placeholder="Buscar (Ej. pais, estado, ciudad, calle, codigo postal)"
+        />
+      </View>
+      <FlatList
+        data={places}
+        renderItem={renderItem}
+        ItemSeparatorComponent={Separator}
+        keyExtractor={(item) => item.place_id + ""}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: { 
-        paddingTop: 50, 
-        flex: 1, 
-        backgroundColor: 'whitesmoke'},
-    input: {
-        backgroundColor: 'white',
-        flexDirection: 'row', 
-        marginHorizontal: 10,
-        padding: 8,
-        borderRadius: 20,
-        alignItems: 'center',
-        marginBottom: 10
-    },
-    textInputStyle: {
-        flex: 1,
-        paddingLeft: 5
-    },
-    separator: {
-        backgroundColor: "rgba(0,0,0,0.22)",
-        height: .5,
-    }
-})
+  container: {
+    paddingTop: 50,
+    flex: 1,
+    backgroundColor: "whitesmoke",
+  },
+  input: {
+    backgroundColor: "white",
+    flexDirection: "row",
+    marginHorizontal: 10,
+    padding: 8,
+    borderRadius: 20,
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  textInputStyle: {
+    flex: 1,
+    paddingLeft: 5,
+    paddingVertical: 1,
+  },
+  separator: {
+    backgroundColor: "rgba(0,0,0,0.22)",
+    height: 0.5,
+  },
+});
