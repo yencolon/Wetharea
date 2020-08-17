4f971f9d903635f2ec2c728d094b2ddfa52d8dee
Manuel Meneses <manuelmenesesg@hotmail.com>
2020-08-17 15:15:55 -0400

    Adding loading screen

--- App.js
+++ App.js
@@ -1,44 +1,33 @@
-import React, { useState , useEffect } from "react";
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
 // Screen
+import InitialScreen from "./app/screen/InitialScreen";
+import SearchLocation from "./app/screen/SearchLocation";
 import Home from "./app/screen/Home";
-import SearchLocation from './app/screen/SearchLocation'
-import { localStorage } from "./app/storage/localStorage";
-import { Header } from "react-native/Libraries/NewAppScreen";
-import { View } from "react-native";
 
 const Stack = createStackNavigator();
-
 export default function App() {
-  const [latitude, setLatitude] = useState(0);
-  const [longitude, setLongitude] = useState(0);
-  
-
-  useEffect(() => {
-    
-    const getCurrentWeather = async() => {
-      const currentWeather = (await localStorage.getForecast())
-    }
-
-    getCurrentWeather()
-    return () => {
-
-    }
-  }, [])
-
-  const setLocation = (latitude, longitude) => {
-    setLatitude(latitude)
-    setLongitude(longitude)
-  }
-
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
-          <Stack.Screen name="SearchLocation" component={SearchLocation}  options={{headerShown: false}}/>
-          <Stack.Screen name="Home" component={Home} options={{headerShown: false}}/>
+      <Stack.Navigator initialRouteName={InitialScreen}>
+        <Stack.Screen
+          name="InitialScreen"
+          component={InitialScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SearchLocation"
+          component={SearchLocation}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
-  )
+  );
 }
---
+++ app/screen/InitialScreen.js
@@ -0,0 +1,44 @@
+import React, { useEffect } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { StackActions } from '@react-navigation/native';
+
+// Components
+import ActivityIndicatorApp from '../components/ActivityIndicatorApp'
+
+// LocalStorgae
+import { localStorage } from "../storage/localStorage";
+
+const InitialScreen = ({ navigation }) => {
+
+  useEffect(() => {
+    const getCurrentWeather = async () => {
+      const currentWeather = await localStorage.getForecast();
+      if(!currentWeather){
+        navigation.dispatch(StackActions.replace('SearchLocation'))
+      } else {
+        navigation.dispatch(StackActions.replace('Home'))
+      }
+    };
+
+    getCurrentWeather();
+  }, []);
+
+    return (
+        <View style={styles.container}>
+            <Text>¡ W E T H A R E A !</Text>
+            <ActivityIndicatorApp />
+            <Text style={{ fontSize: 15}}>Cargando ..</Text>
+        </View>
+    )
+}
+
+export default InitialScreen
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: '#b3d9f2'
+    }
+})
