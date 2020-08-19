40108c03e849d4a449e64a4351af555f5d77e9b5
Manuel Meneses <manuelmenesesg@hotmail.com>
2020-08-19 18:02:40 -0400

    Changing background color

--- app/screen/InitialScreen.js
+++ app/screen/InitialScreen.js
@@ -39,6 +39,6 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-        backgroundColor: '#b3d9f2'
+        backgroundColor: 'whitesmoke'
     }
 })
