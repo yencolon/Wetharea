fe195d268644f4bb1dbd252fdb9a556cbefaf2f2
Manuel Meneses <manuelmenesesg@hotmail.com>
2020-08-20 15:44:01 -0400

    Formatting code

--- app/api/index.js
+++ app/api/index.js
@@ -1,4 +1,5 @@
 import { API_URL, API_KEY, API_GEOCODING, API_REVERSE_GEOCODING } from "./env";
+
 export const weather = {
   async getWeather(lat, long) {
     return await fetch(
@@ -10,13 +11,9 @@ export const weather = {
 
 export const searchLoaction = {
   async getPlaces(place) {
-    return await fetch(
-      `${API_GEOCODING}${place}&format=json&limit=10`
-    );
+    return await fetch(`${API_GEOCODING}${place}&format=json&limit=10`);
   },
-  async getReverseLocation(lat, lon){ 
-    return await fetch(
-      `${API_REVERSE_GEOCODING}lat=${lat}&lon=${lon}&zoom=10`
-    )
-  }
+  async getReverseLocation(lat, lon) {
+    return await fetch(`${API_REVERSE_GEOCODING}lat=${lat}&lon=${lon}&zoom=10`);
+  },
 };
