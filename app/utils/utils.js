e2a13504afdaf3f8d80c545ab508ff7edd2a63b5
Manuel Meneses <manuelmenesesg@hotmail.com>
2020-08-24 19:41:09 -0400

    Inproving time functions

--- app/utils/utils.js
+++ app/utils/utils.js
@@ -1,15 +1,21 @@
-export const whichBackground = () => {
-  const date = new Date();
-  if (date.getHours() >= 4 && date.getHours() < 6) {
+export const whichBackground = (date_, timezoneOffset) => {
+  const time = new Date(date_ * 1000);
+  time.setUTCSeconds(timezoneOffset + time.getTimezoneOffset() * 60);
+  if (time.getHours() >= 4 && time.getHours() < 6) {
     return require("../assets/img/dawn-bg.jpg");
   }
-  if (date.getHours() >= 6 && date.getHours() < 15) {
+  if (time.getHours() >= 6 && time.getHours() < 15) {
     return require("../assets/img/day-bg.jpg");
   }
-  if (date.getHours() >= 16 && date.getHours() < 19) {
+  if (time.getHours() >= 16 && time.getHours() < 19) {
     return require("../assets/img/noon-bg.jpg");
   }
-  return require("../assets/img/night-bg.jpg");
+  if (
+    (time.getHours() >= 19 && time.getHours() < 23) ||
+    (time.getHours() >= 0 && time.getHours() < 4)
+  ) {
+    return require("../assets/img/night-bg.jpg");
+  }
 };
 
 export const kelvinToCelsius = (measure) => {
@@ -25,35 +31,10 @@ export const filterHoursActualDay = (hourly) => {
   return hours;
 };
 
-export const getHoursToday = (date, timezoneOffset) => {  
-  
-  const time =  new Date(date * 1000);
+export const getHoursToday = (date, timezoneOffset) => {
+  const time = new Date(date * 1000);
   time.setUTCSeconds(timezoneOffset + time.getTimezoneOffset() * 60);
-
-  if (
-    time.getHours().toString().length > 1 &&
-    time.getMinutes().toString().length > 1
-  ) {
-    return `${time.getHours()}:${time.getMinutes()}`;
-  }
-  if (
-    time.getHours().toString().length > 1 &&
-    time.getMinutes().toString().length === 1
-  ) {
-    return `${time.getHours()}:0${time.getMinutes()}`;
-  }
-  if (
-    time.getHours().toString().length === 1 &&
-    time.getMinutes().toString().length > 1
-  ) {
-    return `0${time.getHours()}:${time.getMinutes()}`;
-  }
-  if (
-    time.getHours().toString().length === 1 &&
-    time.getMinutes().toString().length === 1
-  ) {
-    return `0${time.getHours()}:0${time.getMinutes()}`;
-  }
+  return time.toLocaleTimeString().slice(0, 5);
 };
 
 export const getUVIndex = (uvIndex) => {
@@ -67,52 +48,51 @@ export const getUVIndex = (uvIndex) => {
 
 //Envia el main del objeto weather retorna un simbolo.
 export const getWeatherIcon = (weatherIcon) => {
-
   switch (weatherIcon) {
-    case '01d':
-      return  {icon1: "☀", icon2: ''};
-    case '01n':
-      return {icon1: '🌕', icon2: ''};
+    case "01d":
+      return { icon1: "☀", icon2: "" };
+    case "01n":
+      return { icon1: "🌕", icon2: "" };
 
-    case '02d':
-      return {icon1: "⛅", icon2: ''};
-    case '02n':
-      return {icon1: "🌕", icon2: '☁'};
+    case "02d":
+      return { icon1: "⛅", icon2: "" };
+    case "02n":
+      return { icon1: "🌕", icon2: "☁" };
 
-    case '03d':
-      return {icon1: "☁", icon2: ''};
-    case '03n':
-      return {icon1: "☁", icon2: ''};
+    case "03d":
+      return { icon1: "☁", icon2: "" };
+    case "03n":
+      return { icon1: "☁", icon2: "" };
 
     case "04d":
-      return {icon1: "☁", icon2: '☁'};
+      return { icon1: "☁", icon2: "☁" };
     case "04n":
-      return {icon1: "☁", icon2: '☁'};
+      return { icon1: "☁", icon2: "☁" };
 
     case "09d":
-      return {icon1: "🌧", icon2: ''};
+      return { icon1: "🌧", icon2: "" };
     case "09n":
-      return {icon1: "🌧", icon2: ''};
+      return { icon1: "🌧", icon2: "" };
 
     case "10d":
-      return {icon1: "🌦", icon2: ''};
+      return { icon1: "🌦", icon2: "" };
     case "10n":
-      return {icon1: "🌕", icon2: '🌧'};
+      return { icon1: "🌕", icon2: "🌧" };
 
     case "11d":
-      return {icon1: "⛈", icon2: ''};
+      return { icon1: "⛈", icon2: "" };
     case "11n":
-      return {icon1: "⛈", icon2: ''};
+      return { icon1: "⛈", icon2: "" };
 
     case "13d":
-      return {icon1: "🌨", icon2: ''};
+      return { icon1: "🌨", icon2: "" };
     case "13n":
-      return {icon1: "🌨", icon2: ''};
+      return { icon1: "🌨", icon2: "" };
 
     case "50d":
-      return {icon1: "🌫", icon2: ''};
+      return { icon1: "🌫", icon2: "" };
     case "50n":
-      return {icon1: "🌫", icon2: ''};
+      return { icon1: "🌫", icon2: "" };
     default:
       return "";
   }
@@ -123,7 +103,7 @@ export const getDayDateName = (date) => {
 
   return (
     ["Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sábado"][
-    date.getDay()
+      date.getDay()
     ] +
     " " +
     date.getDate()
